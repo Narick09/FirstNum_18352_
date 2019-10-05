2dde6df641fccc70615fe60fee00748c794578f7
void <49158033+Narick09@users.noreply.github.com>
2019-10-05 20:50:55 +0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,42 +1,18 @@
 #include<iostream>
 #include"RNK.h"
 
+
 int main() {
 	setlocale(LC_ALL, "");
 	RNK rna1;
 	size_t size = 25;
 	RNK rna2(size, C);
 	RNK rna3(rna2);
-	for (size_t i = 0; i < rna3.length(); i++)
-		std::cout << rna3[i] << " ";
-	std::cout << "\n";
 
-//	rna3 = rna2;
-	rna3[250] = T;//alloc mem
-	for (size_t i = 0; i < rna3.length(); i++)
-		std::cout << rna3[i] << " ";
-	std::cout << "\n";
+	std::cout << rna2;
 
-	rna1[4] = T;//alloc mem
-	std::cout << rna1.length() << "\n";
-	std::cout << "array:\n";
-	for (size_t i = 0; i < rna1.length(); i++)
-		std::cout << (Nucleotide)rna1[i] << " ";
-	std::cout << "end:\n";
-//	Nucleotide n = rna3[249];			//correct
-//	std::cout << rna3[250] << "\n";
-
-	rna3.trim(45);
-	
-	!rna3;
-	for (size_t i = 0; i < rna3.length(); i++)
-		std::cout << rna3[i] << " ";
-	std::cout << "\n";
-
-	std::cout << "\n" << rna1.isComplementary(rna3);
-
-//	std::cout << "\n" << rna3[2];
-
+	rna3[56] = G;
+	std::cout << rna3;
 
 	system("pause");
 	return 0;
