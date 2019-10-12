01246752d1dfa40fe257e61122d141458d520bfa
void <49158033+Narick09@users.noreply.github.com>
2019-10-12 11:35:55 +0500

    Update RNK.h

--- RNK.h
+++ RNK.h
@@ -5,22 +5,19 @@ enum Nucleotide { A, G, C, T };
 
 class RNK {
 private:
-	//   sizeof(size_t)
 	friend class reference;
 	class reference
 	{
-		RNK *rna;				//why not "&"?
+		RNK *rna;
 		size_t ind;
 	public:
 		reference(RNK * const ptr, size_t ind);
 		reference & operator=(Nucleotide nucleotide);
 		operator Nucleotide() const;//just "toType" - operator overload
-		//Can we write operator overload in "RNK", but not in "ref."? What will changed after that?
-		//It's will be problem with rna[i] = Nucl;
 	};
 	size_t size_;
 	size_t *Nuc_;
-	size_t physSize; // actual physSize 
+	size_t physSize; // actual physical Size 
 public:
 	RNK() : Nuc_(nullptr), size_(0), physSize(0) {}
 	RNK(size_t s, Nucleotide n);
@@ -43,15 +40,14 @@ public:
 	//для нуклеотида  - число значений
 	size_t cardinality(Nucleotide value) const;
 	//аналогично но сразу для всех типов тритов
-	std::unordered_map < Nucleotide, int, std::hash<int> > cardinality() const;//capacity(?)
-//------------------------------------------------------------------------
-//	Nucleotide TmpGeterNuc(size_t i) const;
+	std::unordered_map < Nucleotide, int, std::hash<int> > cardinality() const;
+
 	RNK & operator=(const RNK & rna);
 
 	reference operator[](size_t ind);
 	Nucleotide operator[](size_t ind) const;
 	friend std::ostream& operator << (std::ostream & out, const RNK& other);
+//	Nucleotide TmpGeterNuc(size_t i) const;						//for debag							//for debag
+//	size_t getPSize() const{ return this->physSize;}
 	~RNK();
 };
-
-//rha[1000001]; - check time
